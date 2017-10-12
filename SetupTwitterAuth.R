5f08c850a31c5ca156de74c80ac1ef314dd5cd55
vinitg91 <vinitg91@gmail.com>
2017-10-12 10:51:39 -0400

    Update
    Removed my private keys/tokens and replaced them with placeholder for user's own keys/token to connect with Twitter API.

--- SetupTwitterAuth.R
+++ SetupTwitterAuth.R
@@ -3,10 +3,11 @@ install_github("geoffjentry/twitteR")
 library(devtools)
 library(twitteR)
 
-consumer_key = "aKhkJgQPbzVWle6t8CK8FqLAl"
-consumer_secret = "OlYwBlKSjyaqjB4t8UktTkATaeRczeujFIwRePQFywJ86Fw7RK"
-access_token = "64362382-jZaXuBrYFjrcN3UqrNqn92pdZwHiSDA7tcMuVBF17"
-access_secret = "e0d1tqGqPkeREg0Wveoy1UjNiycDGS12Fzfge902LqXsl"
+#Get these tokens from your Twitter account to be used here in Twitter API
+consumer_key = "YourConsumerKey"
+consumer_secret = "YourConsumerSecret"
+access_token = "YourAccessToken"
+access_secret = "YourAccessSecret"
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
